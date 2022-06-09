ff93933a77da1610d279504ae0ff076d0b593ca9
idongessien <idong.essien@comcast.net>
2022-06-09 19:25:30 -0400

    Add Madlibs Directory

---
+++ madlibs/madlibs.py
@@ -0,0 +1,8 @@
+adjective = input("adjective: ")
+verb_one = input("verb: ")
+verb_two = input("verb: ")
+famous_person = input("first name of famous person: ")
+
+madlib = f"I love learning computer programming, it's so {adjective}! I get so hype because I like to {verb_one} stuff. Stay learnt and {verb_two} like you're {famous_person}!"
+
+print(madlib)
